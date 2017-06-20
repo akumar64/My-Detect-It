6e3a8614d79f2c1a5760b756f1fa21e16261ec1f
Arjun <astro.arjun89@gmail.com>
2017-06-20 12:13:20 -0700

    Initial Commit

---
+++ CodingChallenge/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CodingChallenge
+//
+//  Created by ARJUN KUMAR on 6/20/17.
+//  Copyright © 2017 ARJUN KUMAR. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
